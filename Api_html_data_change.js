d3c8a6ba71bf5f8658b9aa374dabd3b155934d26
Chetan Salvi <96784802+ChetanSalvi7@users.noreply.github.com>
2022-06-17 09:38:15 +0530

    Html tag ko valid krna react me

---
+++ Api_html_data_change.js
@@ -0,0 +1,6 @@
+// jab api data me (lorem <b>ipsum</b>') is trh k tag aarhe h to use react me show krne k liye iska use kiya jata h.
+
+
+<div
+      dangerouslySetInnerHTML={{__html: data}}
+    />
